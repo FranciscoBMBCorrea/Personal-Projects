1a9158f1b69963b4fa4b98eaf1f6538183dd4137
FranciscoBMBCorrea <franciscobmbc@gmail.com>
2026-04-14 22:48:45 -0300

    feat: add localized project routes

---
+++ Portfolio/src/app/[locale]/projects/[slug]/page.tsx
@@ -0,0 +1,49 @@
+import { notFound } from 'next/navigation'
+
+import { ProjectPage } from '@/components/project-page'
+import {
+  getPortfolioCopy,
+  getProject,
+  isLocale,
+  locales,
+  projects,
+  type Locale,
+} from '@/data/portfolio'
+
+type Props = {
+  params: Promise<{
+    locale: string
+    slug: string
+  }>
+}
+
+export async function generateStaticParams() {
+  return locales.flatMap((locale) =>
+    projects.map((project) => ({
+      locale,
+      slug: project.slug,
+    })),
+  )
+}
+
+export default async function ProjectRoutePage({ params }: Props) {
+  const { locale, slug } = await params
+
+  if (!isLocale(locale)) {
+    notFound()
+  }
+
+  const project = getProject(locale as Locale, slug)
+
+  if (!project) {
+    notFound()
+  }
+
+  return (
+    <ProjectPage
+      locale={locale as Locale}
+      copy={getPortfolioCopy(locale as Locale)}
+      project={project}
+    />
+  )
+}

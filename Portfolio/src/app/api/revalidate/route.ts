274dd7b23361e02c01932bf9e5fd458f884fa487
FranciscoBMBCorrea <franciscobmbc@gmail.com>
2026-04-14 22:45:36 -0300

    api: update revalidation route

--- Portfolio/src/app/api/revalidate/route.ts
+++ Portfolio/src/app/api/revalidate/route.ts
@@ -1,8 +1,10 @@
-import { NextResponse } from "next/server";
-import { revalidateTag } from "next/cache";
+import { revalidateTag } from 'next/cache'
+import { NextResponse } from 'next/server'
+
+export const dynamic = 'force-dynamic'
 
 export async function POST() {
-  revalidateTag("prismic");
+  revalidateTag('prismic', 'max')
 
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  return NextResponse.json({ revalidated: true, now: Date.now() })
 }
